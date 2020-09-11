c76238e5181d3dfa61136281f763e641f1bfdb31
Mario Vanegas <mario.vanegas9090@gmail.com>
2020-09-11 11:29:26 -0600

    ✨Classifies HotDog or not

--- HotDogOrNot/Source/ViewController.swift
+++ HotDogOrNot/Source/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var hotdogView: UIView!
     @IBOutlet weak var noHotdogView: UIView!
     @IBOutlet weak var cameraButton: UIButton!
+    
+    private lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            let squeezeNet = SqueezeNet()
+            let visionModel = try VNCoreMLModel(for: squeezeNet.model)
+            let request = VNCoreMLRequest(model: visionModel, completionHandler: classificationHandler)
+            
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Error while loading ML Model: \(error)")
+        }
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,8 +57,46 @@ class ViewController: UIViewController {
         present(picker, animated: true)
     }
     
-    func classify(_ image: UIImage) {
+    private func classificationHandler(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async { [weak self] in
+            if let error = error {
+                print("There was an error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let results = request.results as? [VNClassificationObservation], let objectDetected = results.first else {
+                print("There's no results")
+                return
+            }
+            
+            let objectNames = objectDetected.identifier.split(separator: ",")
+            
+            if objectNames.first == "hotdog" {
+                UIView.animate(withDuration: 0.3, animations: { self?.hotdogView.isHidden = false })
+            } else {
+                UIView.animate(withDuration: 0.3, animations: { self?.noHotdogView.isHidden = false })
+            }
+        }
+    }
+    
+    private func classify(_ image: UIImage) {
+        guard let ciImage = CIImage(image: image) else {
+            print("Unable to create CIImage")
+            return
+        }
         
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            guard let self = self else { return }
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+            
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                print("Failed to perform classification request: \(error.localizedDescription)")
+            }
+        }
     }
 }
 
@@ -53,6 +106,7 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         imageView.image = image
         
         picker.dismiss(animated: true)
+        setupViews()
         classify(image)
     }
 }
