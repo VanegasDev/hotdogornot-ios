526641b508459c085f8eae192e5b1d16ae9beb77
Mario Vanegas <mario.vanegas9090@gmail.com>
2020-09-11 05:43:23 -0600

    💄 Designs application view

--- HotDogOrNot/Source/ViewController.swift
+++ HotDogOrNot/Source/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var hotdogView: UIView!
+    @IBOutlet weak var noHotdogView: UIView!
+    @IBOutlet weak var cameraButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupCamera()
+        setupViews()
     }
+    
+    @IBAction func performCameraAction(_ sender: Any) {
+        presentPhotoPicker(.camera)
+    }
+    
+    private func setupCamera() {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    private func setupViews() {
+        hotdogView.isHidden = true
+        noHotdogView.isHidden = true
+    }
+    
+    private func presentPhotoPicker(_ sourceType: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        
+        present(picker, animated: true)
+    }
+    
+    func classify(_ image: UIImage) {
+        
+    }
+}
 
-
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as! UIImage
+        imageView.image = image
+        
+        picker.dismiss(animated: true)
+        classify(image)
+    }
 }
 
