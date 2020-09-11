1e9d0af5e282ea5ed37f21c2d46b58e0ae60959d
Mario Vanegas <mario.vanegas9090@gmail.com>
2020-09-11 04:41:56 -0600

    🎉 Initializes HotDogOrNot project

---
+++ HotDogOrNot/Source/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotDogOrNot
+//
+//  Created by Mario Vanegas on 9/11/20.
+//  Copyright © 2020 vanegasmario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
